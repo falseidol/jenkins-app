2ae2aa81fec7544946437884babcbd8cb1bb32c9
falseidol <chuchuk.tima@mail.ru>
2023-05-30 16:43:22 +0300

    hello 19!

--- src/main/java/aston/HelloServlet.java
+++ src/main/java/aston/HelloServlet.java
@@ -1,13 +1,13 @@
 package aston;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping
 public class HelloServlet {
-    @GetMapping("/hello")
+    @GetMapping
     public String helloWorld() {
         return "Hello world8!";
     }
