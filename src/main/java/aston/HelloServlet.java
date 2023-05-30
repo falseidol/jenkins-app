e5cf290abe62d0e96aa60fd7798460e604e7fb79
falseidol <chuchuk.tima@mail.ru>
2023-05-30 16:43:35 +0300

    hello 20!

--- src/main/java/aston/HelloServlet.java
+++ src/main/java/aston/HelloServlet.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloServlet {
     @GetMapping
     public String helloWorld() {
-        return "Hello world8!";
+        return "Hello world9!";
     }
 }
